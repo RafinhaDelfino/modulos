cc9c728800d342736df114e23a6577955bdacace
Rafael <rafael.delfino2006@gmail.com>
2023-08-29 15:32:01 -0300

    RafinhaDelfino

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicar = require("./modulos/multiplicar");
+var diminuir = require("./modulos/diminuir");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multiplicar(20,90));
+console.log(diminuir(20,90));
